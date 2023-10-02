f585cf42ff65b98553f3875409d49580aba234cf
spanzy teo <spanzytheo@gmail.com>
2023-10-02 19:51:37 +0100

    changes made to exercise
    description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,21 @@ import HeroBanner from '../components/HeroBanner'
 import Exercises from '../components/Exercises'
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([])
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   )
 }
